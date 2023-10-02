601dfefc026f5d92b3c7931df19550146d835c88
guvenkarabulut <guven.07.568@gmail.com>
2023-10-02 19:34:49 +0300

    initial commit

---
+++ sp-spotify-playlist-adder/src/main/java/com/guvenkarabulut/spspotifyplaylistadder/SpSpotifyPlaylistAdderApplication.java
@@ -0,0 +1,13 @@
+package com.guvenkarabulut.spspotifyplaylistadder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSpotifyPlaylistAdderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpSpotifyPlaylistAdderApplication.class, args);
+	}
+
+}
---
+++ sp-spotify-playlist-adder/src/test/java/com/guvenkarabulut/spspotifyplaylistadder/SpSpotifyPlaylistAdderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guvenkarabulut.spspotifyplaylistadder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpSpotifyPlaylistAdderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
